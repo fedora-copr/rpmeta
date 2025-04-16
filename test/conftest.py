4e426f000af82fb1b1e47f8274f112407a6e8a9f
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2025-04-16 17:32:45 +0200

    tests: integration testing for fetcher

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,11 @@
+import json
 from pathlib import Path
 from subprocess import Popen
 from time import sleep
 
 import pytest
 
+from rpmeta.dataset import HwInfo, Record
 from test.helpers import run_rpmeta_cli
 
 
@@ -38,3 +40,40 @@ def api_server(trained_model):
     sleep(3)
     yield
     api_server.terminate()
+
+
+@pytest.fixture
+def koji_build():
+    with open(Path(__file__).parent / "data" / "koji_build.json") as f:
+        return json.load(f)
+
+
+@pytest.fixture
+def koji_task_descendant():
+    with open(Path(__file__).parent / "data" / "koji_task_descendant.json") as f:
+        return json.load(f)
+
+
+@pytest.fixture
+def hw_info():
+    return HwInfo(
+        cpu_arch="x86_64",
+        cpu_cores=4,
+        cpu_model="12",
+        cpu_model_name="silny procak",
+        ram=16,
+        swap=8,
+    )
+
+
+@pytest.fixture
+def dataset_record(hw_info, koji_build):
+    return Record(
+        package_name=koji_build[0]["name"],
+        version=koji_build[0]["version"],
+        release=koji_build[0]["release"],
+        epoch=0,
+        mock_chroot="fedora-43-x86_64",
+        build_duration=893,
+        hw_info=hw_info,
+    )
